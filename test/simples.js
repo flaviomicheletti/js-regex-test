cd5408c412efe93684f413dda83eec1db81f4fed
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-03-16 21:30:02 -0300

    test/simple

--- test/simples.js
+++ test/simples.js
@@ -1,18 +1,18 @@
 var expect = require("expect.js");
 
-describe('regex', function() {
-   describe('simples', function() {
+describe('simples', function() {
+   describe('replace', function() {
 
        //
        //    /-/g
        //
        it('deve retirar os traços', function() {
 
-           var value = "a-b-c".replace(/-/g, "");
-           expect(value).to.equal('abc');
+            var value = "a-b-c".replace(/-/g, "");
+            expect(value).to.equal('abc');
 
-           var value = "2019-04-27".replace(/-/g, "");
-           expect(value).to.equal('20190427');
+            var value = "2019-04-27".replace(/-/g, "");
+            expect(value).to.equal('20190427');
 
        })
    })
